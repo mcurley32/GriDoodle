0f41527879473e2c659ce86cd1c03dfd88448fa4
MikeCurley <mcurley32@gmail.com>
2016-05-28 22:34:56 -0400

    Completed Project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
   var gridCount = 16;
-  var gridSize = 960;
-  var $container = $('div#container');
-  
+  var gridSize = 720;
+  var cellSize = Math.floor( gridSize / gridCount );
+  for (var i = 0; i < gridCount*gridCount; i++){
+    $('div#container').append('<div class="pixel"></div>');
+  };
+  $('div.pixel').width(cellSize+"px");
+  $('div.pixel').height(cellSize+"px");
+  $('div.pixel').hover(function(){
+    $(this).addClass('hovered');
+  });
+  $('button').click(function(){
+    gridCount = prompt("How many cells do you want in your new sketch grid?");
+    gridSize = 720;
+    $('div#container').empty();
+    cellSize = Math.floor( gridSize / gridCount );
+    gridSize = cellSize * gridCount;
+    $('div#container').width(gridSize+"px");
+    $('div#container').height(gridSize+"px");
+    for (var i = 0; i < gridCount*gridCount; i++){
+      $('div#container').append('<div class="pixel"></div>');
+    };
+    $('div.pixel').width(cellSize+"px");
+    $('div.pixel').height(cellSize+"px");
+    $('div.pixel').hover(function(){
+      $(this).addClass('hovered');
+    });
+  });
 });
